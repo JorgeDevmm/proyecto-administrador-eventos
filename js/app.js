545888a26a5fcb53c4679d11b9e720734f650384
JorgeDevmm <jorge29luis10@gmail.com>
2023-12-13 13:28:09 -0500

    Mostrar los eventos en HTML y eliminarlos: funciones imprimirEvento, eliminarEventosHTML

--- js/app.js
+++ js/app.js
@@ -24,9 +24,15 @@ class Eventos {
 
     console.log(this.eventos);
   }
+
+  // guardo en un nuevo array sin el id a eliminar
+  eliminarEvento(id) {
+    this.eventos = this.eventos.filter((evento) => evento.id != id);
+  }
 }
 
 class InterfazUsuario {
+  // Método que muestra un alerta segun su tipo
   imprimirAlerta(mensaje, tipo) {
     // Crear Div
     const divMensaje = document.createElement('div');
@@ -51,6 +57,71 @@ class InterfazUsuario {
     setTimeout(() => {
       divMensaje.remove();
     }, 5000);
+  }
+
+  // Imprime el evento en el html, despues de ingresarlo, destructurandolo en el arreglo
+  imprimirEvento({ eventos }) {
+    // limpiamos los elementos del contenedorEventos para evitar duplicidad
+    this.limpiarHtml(contenedorEventos);
+    // iteramos el arreglo de eventos
+    eventos.forEach((eventoObj) => {
+      // Aplicar destructuración, extraemos la información del objeto de evento
+      const { evento, contacto, telefono, fecha, hora, agenda, id } = eventoObj;
+
+      const divEvento = document.createElement('div');
+      divEvento.classList.add('evento', 'p-3');
+      divEvento.dataset.id = id;
+
+      // Scripting de los elementos del evento
+      const eventoParrafo = document.createElement('h2');
+      eventoParrafo.classList.add('card-title', 'font-weight-bolder');
+      eventoParrafo.textContent = evento;
+
+      const contactoParrafo = document.createElement('p');
+      contactoParrafo.innerHTML = `<span class="font-weight-bolder">Contacto: </span> ${contacto}`;
+
+      const telefonoParrafo = document.createElement('p');
+      telefonoParrafo.innerHTML = `<span class="font-weight-bolder">Telf: </span> ${telefono}`;
+
+      const fechaParrafo = document.createElement('p');
+      fechaParrafo.innerHTML = `<span class="font-weight-bolder">Fecha: </span> ${fecha}`;
+
+      const horaParrafo = document.createElement('p');
+      horaParrafo.innerHTML = `<span class="font-weight-bolder">Hora: </span> ${hora}`;
+
+      const agendaParrafo = document.createElement('p');
+      agendaParrafo.innerHTML = `<span class="font-weight-bolder">Agenda: </span> ${agenda}`;
+
+      const btnEliminiar = document.createElement('button');
+      btnEliminiar.classList.add('btn-danger', 'p-2');
+      btnEliminiar.textContent = 'Eliminar';
+      btnEliminiar.value = id;
+
+      btnEliminiar.addEventListener('click', () => {
+        eliminarEventosHTML(id);
+      });
+
+      // Agregar los parrafos al divEvento
+      divEvento.appendChild(eventoParrafo);
+      divEvento.appendChild(contactoParrafo);
+      divEvento.appendChild(telefonoParrafo);
+      divEvento.appendChild(fechaParrafo);
+      divEvento.appendChild(horaParrafo);
+      divEvento.appendChild(agendaParrafo);
+      divEvento.appendChild(btnEliminiar);
+
+      // Agregar los eventos al html
+      contenedorEventos.appendChild(divEvento);
+    });
+  }
+
+  // Eliminar elementos de la referencia de alertas
+  limpiarHtml(referencia) {
+    // si contenedor tiene al menos un elemento
+    while (referencia.firstChild) {
+      // eliminar un hijo por el primero
+      referencia.removeChild(referencia.firstChild);
+    }
   }
 }
 
@@ -92,7 +163,7 @@ function datosEvento(e) {
 function nuevoEvento(e) {
   e.preventDefault();
 
-  // Aplicar destructuración, extraemos la información del obejto de cita
+  // Aplicar destructuración, extraemos la información del objeto de evento
   const { evento, contacto, telefono, fecha, hora, agenda } = eventoObj;
 
   // Validar
@@ -118,13 +189,16 @@ function nuevoEvento(e) {
   adinistrarEventos.agregarEvento({ ...eventoObj });
 
   // Reiniciar el objeto para la validación
-  reinicarObjeto();
+  reiniciarObjeto();
 
   // Reiniciar el formulario
   formulario.reset();
+
+  // Mostrar el HTML de los eventos, de acuerdo al arreglo
+  interfazUsuario.imprimirEvento(adinistrarEventos);
 }
 
-function reinicarObjeto() {
+function reiniciarObjeto() {
   // borramos los valores del objeto prinicipal
   eventoObj.evento = '';
   eventoObj.contacto = '';
@@ -132,4 +206,11 @@ function reinicarObjeto() {
   eventoObj.fecha = '';
   eventoObj.hora = '';
   eventoObj.agenda = '';
+}
+
+function eliminarEventosHTML(id) {
+  // paso el id de referencia a eliminar y devuelvo el nuevo arreglo sin el id a eliminar
+  adinistrarEventos.eliminarEvento(id);
+  // imprimimos los eventos nuevamente, sin el id eliminado de acuero al array del objeto
+  interfazUsuario.imprimirEvento(adinistrarEventos);
 }
